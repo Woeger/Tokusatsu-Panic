ada93af8b0c93a42d45e1195f50cfbd76ccfa557
Thomas Sutton <thomas.sutton00@sky.com>
2023-01-24 15:42:01 +0000

    Added additional blueprint functions + template

--- TokusatsuPanic/Private/Items/Item.cpp
+++ TokusatsuPanic/Private/Items/Item.cpp
@@ -18,6 +18,16 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 }
 
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
@@ -25,11 +35,14 @@ void AItem::Tick(float DeltaTime)
 
 	RunningTime += DeltaTime;
 
-	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
 
-	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
 
 	DRAW_SPHERE_SINGLEFRAME(GetActorLocation());
 	DRAW_VECTOR_SINGLEFRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+
+	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	DRAW_POINT_SINGLEFRAME(AvgVector);
 }
 
