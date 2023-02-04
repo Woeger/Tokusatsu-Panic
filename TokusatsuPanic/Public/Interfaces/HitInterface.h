afc9ba87f138467d48c80f50f41dadfad47a9f09
Thomas Sutton <thomas.sutton00@sky.com>
2023-02-04 22:08:27 +0000

    Added enemy

--- TokusatsuPanic/Public/Interfaces/HitInterface.h
+++ TokusatsuPanic/Public/Interfaces/HitInterface.h
@@ -20,6 +20,7 @@ class TOKUSATSUPANIC_API IHitInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	virtual void GetHit() = 0;
 };
